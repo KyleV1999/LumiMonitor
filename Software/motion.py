e3ba6364a2e231363d93039937617716c9a3e584
KyleV1999 <54862039+KyleV1999@users.noreply.github.com>
2019-11-10 17:48:26 -0500

    Update motion.py

--- Software/motion.py
+++ Software/motion.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 PIR_input = 12
 GPIO.setwarnings(False)
@@ -6,6 +7,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIR_input,GPIO.IN)
 
 while True:
+    time.sleep(0.1)
     if(GPIO.input(PIR_input)):
         print("Motion detected")
     else:
